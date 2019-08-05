02d6a54f4913c18a12a53a7459dba2c01c4a65cd
Leonardo A. Pérez Castilla <ephelsa@hotmail.com>
2019-08-04 23:27:05 -0500

    Terminado: Ingreso de Automata. ~ Mostrar automata.

---
+++ 1. Automata/iniciar.sh
@@ -0,0 +1,6 @@
+echo "-> Instalando dependencias."
+pip3 install -r proyecto/dependencias.txt
+clear
+echo "-> Dependencias instaladas."
+echo "-> Ejecutando proyecto."
+python3 proyecto/__init__.py
--- 1. Automata/proyecto/automata/Automata.py
+++ 1. Automata/proyecto/automata/Automata.py
@@ -1,3 +1,5 @@
+from terminaltables import AsciiTable
+
 from tokenizer.Tokenizer import tokenizerSimple
 from tokenizer.Tokenizer import tokenizerTransiciones
 
@@ -9,10 +11,10 @@ class Automata:
     '''
     Atributos necesarios para la creacion de la tabla de transiciones y validacion del automata.
     '''
-    simbolos_entrada = {}
-    estados = {}
-    estado_inicial = {}
-    estados_aceptacion = {}
+    simbolos_entrada = []
+    estados = []
+    estado_inicial = []
+    estados_aceptacion = []
 
     '''
     Constructor
@@ -20,10 +22,10 @@ class Automata:
     def __init__(self, simbolos_entrada, estados, estado_inicial, estados_aceptacion):
         self.simbolos_entrada = simbolos_entrada
         self.estados = estados
-        self.estado_inicial = estado_inicial 
+        self.estado_inicial = estado_inicial[0]
         self.estados_aceptacion = estados_aceptacion
 
-        self.tabla_transiciones = self.TablaTransiciones()
+        self.tabla_transiciones = self.TablaTransiciones(self.simbolos_entrada, self.estado_inicial)
 
         # Array con todas las filas de transiciones.
         filas_transiciones = []
@@ -31,15 +33,15 @@ class Automata:
             filas_transiciones.append(tokenizerTransiciones(input("Fila[{0}] >> ".format(index + 1))))
 
         #print(filas_transiciones)
-        print(self.automataValido(filas_transiciones))
+        if self.__automataValido(filas_transiciones):
+            self.tabla_transiciones.mostrar()
 
-### Before
-    def automataValido(self, filas_transiciones):
+
+    def __automataValido(self, filas_transiciones):
         ################ 
         inicial_existente = False
         aceptacion_existente = []
         estados_existentes = []
-
 
         ################ Total de filas congruente con los estados
         if len(self.estados) != len(filas_transiciones):
@@ -67,7 +69,7 @@ class Automata:
                     return False
 
                 ################# Estado inicial
-                if fila['estado_actual'] == self.estado_inicial[0]:
+                if fila['estado_actual'] == self.estado_inicial:
                     inicial_existente = True
             
             #################### Estados de aceptacion
@@ -81,12 +83,12 @@ class Automata:
                 entradas_existentes.append(False)
 
             #################### Simbolos de entrada correspondientes a la fila
-            fila_entradas = []
+            fila_entradas = {}
             for (i_entrada, entrada) in enumerate(self.simbolos_entrada):
                 if fila[entrada]:
                     entradas_existentes[i_entrada] = True
 
-                    fila_entradas.append({entrada : fila[entrada]})
+                    fila_entradas.update({entrada : fila[entrada]})
 
             #### Insertamos la fila en la tabla de transiciones
             self.tabla_transiciones.insertarFila(fila['estado_actual'], fila_entradas, fila['estado_actual'] in self.estados_aceptacion)
@@ -96,12 +98,12 @@ class Automata:
                     print("Error en los simbolos de entrada en secuencia #{0}".format(str(i_existente + 1)))
                     return False
 
-            print("Entrada " + fila['estado_actual'] + "=> " + str(entradas_existentes))
+            #print("Entrada " + fila['estado_actual'] + "=> " + str(entradas_existentes))
         
         
         ######### Validdamos los otros existentes
         if not inicial_existente:
-            print("El estado inicial {0} no fue encontrado en las secuencias ingresadas.".format(str(self.estado_inicial[0])))
+            print("El estado inicial {0} no fue encontrado en las secuencias ingresadas.".format(str(self.estado_inicial)))
             return False
         
         for (i_aceptacion, aceptacion) in enumerate(aceptacion_existente):
@@ -114,9 +116,9 @@ class Automata:
                 print("El estado {0} no fue encontrado en las secuencias ingresadas".format(str(self.estados[i_estado])))
                 return False
 
-        print("Estados => " + str(estados_existentes))
-        print("Inicial => " + str(inicial_existente))
-        print("Aceptacion  => " + str(aceptacion_existente))
+        #print("Estados => " + str(estados_existentes))
+        #print("Inicial => " + str(inicial_existente))
+        #print("Aceptacion  => " + str(aceptacion_existente))
 
 
         return True
@@ -134,13 +136,74 @@ class Automata:
         transiciones = []
         aceptados = []
 
+        # Tabla de transiciones
+        __tabla_transiciones = []
+
+        '''
+        Constructor: Aqui seteamos la primera fila de la Tabla de Transiciones, la cual corresponde a los simbolos.
+        @param simbolos_entrada Simbolos de entrada que se tienen para la Tabla.
+        @param estado_inicial Estado correspondiente al que ira de primero en la Tabla de Transiciones.
+        '''
+        def __init__(self, simbolos_entrada, estado_inicial):
+            self.__simbolos_entrada = simbolos_entrada
+            self.__estado_inicial = estado_inicial
+
+            entradas = ['CS']
+            for entrada in self.__simbolos_entrada:
+                entradas.append("{0}".format(str(entrada)))
+
+            self.__tabla_transiciones.append(entradas)
+
+
+        '''
+        Insertamos los datos en los arrays de datos para posteriormente actualizar la Tabla de Transiciones.
+
+        @param estado_actual Estado que tiene esa fila actualmente.
+        @param transiciones Son todas las transiciones que posee el estado con el simbolo de entrada.
+        @param aceptado Corresponde al estado que es aceptado por el automata finito.
+        '''
         def insertarFila(self, estado_actual, transiciones, aceptado = False):
             self.estados_actuales.append(estado_actual)
             self.transiciones.append(transiciones)
             self.aceptados.append(aceptado)
 
+            self.__actualizarTabla()
+
+        '''
+        Actualizamos la Tabla de Transiciones ubicando el estado inical en la primera fila (despues de los simbolos).
+        Ademas, los simbolos para cada fila se ubican en el orden que el usuario los haya ingresado.
+        '''
+        def __actualizarTabla(self):
+            nueva_fila = [self.estados_actuales[-1]]
+
+            # Ubicacion de los simbolos en el orden que el usuario digito inicialmente.
+            for entrada in self.__simbolos_entrada:
+                nueva_fila.append((self.transiciones[-1])[entrada])
+
+            nueva_fila.append(self.aceptados[-1])
+
+            # Insercion de la fila en la tabla
+            self.__tabla_transiciones.append(nueva_fila)
+
+            # Modificacion del estado incial.
+            if self.estados_actuales[-1] == self.__estado_inicial:
+                self.__tabla_transiciones[-1], self.__tabla_transiciones[1] = self.__tabla_transiciones[1], self.__tabla_transiciones[-1]
+
+
+        '''
+        Permite obtener el total de transiciones que tiene el estado para X simbolo.
+        @param index Numero de la fila.
+
+        @return Integer Total de transiciones.
+        '''
         def totalTransiciones(self, index):
             return len(self.transiciones[index])
 
+        '''
+        Mostramos la tabla.
+        Se utiliza terminaltables para esto.
+        '''
         def mostrar(self):
-            
+            #print(self.__tabla_transiciones)
+            table = AsciiTable(self.__tabla_transiciones)
+            print(table.table)
