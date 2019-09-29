0e0eb93d8af7a314e716a06acd0b5ed582b7996c
Leonardo Perez <landres.perez@udea.edu.co>
2019-09-29 00:05:27 -0500

    Next set finished

--- 2. Gramaticas/gramatic/gramatic.py
+++ 2. Gramaticas/gramatic/gramatic.py
@@ -20,6 +20,8 @@ Types:
 
 class Gramatic():
     
+    endOfString = '┤'
+
     def __init__(self, gramaticSet):
         # Pattern to get only non-terminals or the left side.
         self.nonTerminalPattern = re.compile(r'^(<[a-z0-9]+>)+$', re.I)
@@ -31,6 +33,8 @@ class Gramatic():
         print('N and null productions:', self.nullSet())
         print('Firts set', self.firstSet())
         print('Firsts set per Production', self.firstProd())
+
+        pprint(self.nextSet())
 
     def nullSet(self):
         # Patter to catch null productions.
@@ -185,7 +189,120 @@ class Gramatic():
         return setOfFirstsProductions
 
     def nextSet(self):
-        pass
+        # Get null productions.
+        nullNonTerminals = list(map(lambda x: x['prod'], self.nullSet()))
+        firstsGroup = self.firstSet()
+
+        # No search again the same nexts per non terminal.
+        foundNonTerm = []
+
+        nextFounds = []
+
+        toSeekAfter = []
+
+        def insertNextFound(nonTerminal, nextFound):
+            if nonTerminal in nextFound:
+                index = nextFounds.index(nextFound)
+                dataToInsert = nextFound[nonTerminal]
+
+                nextFounds[index][nonTerminal].extend(dataToInsert)
+            else:
+                for (indexFound, dataFound) in enumerate(nextFounds):
+                    if nonTerminal in dataFound:
+                        nextFounds[indexFound][nonTerminal].append(nextFound)  
+
+        def findInNextFounds(nonTerminal):
+            for (index, data) in enumerate(nextFounds):
+                if nonTerminal in data:
+
+                    return index
+
+            return -1
+
+
+        # Get the left side per prod.
+        for (indexProd, prod) in enumerate(self.gramaticSet):
+            leftSide = prod[0: str(prod).find('->')]  # Get the left side.
+
+            # Search apparitions.
+            foundArr = []            
+            if leftSide not in foundNonTerm:
+                if indexProd == 0:
+                    nextFounds.append({leftSide: [self.endOfString]})
+
+                for (indexSeek, seekProd) in enumerate(self.gramaticSet):
+                    seekLeftSide = seekProd[0: str(seekProd).find('->')]  # Get the left side.
+                    rightSide = seekProd[str(seekProd).find('->') + 2:]   # Get the rigth side of the prod.
+
+                    if leftSide in rightSide:
+                        foundArr.append({'non_terminal': seekLeftSide, 'right_side': rightSide})
+                        foundNonTerm.append(leftSide)
+
+
+                nextsArr = list(
+                                map(
+                                    lambda x: {
+                                        'non_terminal': x['non_terminal'],
+                                        'next':x['right_side'][(str(x['right_side']).find(leftSide) + len(leftSide)):]
+                                        }, 
+                                    foundArr
+                                )
+                            )
+
+                for nextData in nextsArr:
+                    next2NonTerminal = re.match(re.compile(r'^(<[a-z0-9]+>)+', re.I), nextData['next'])
+                    next2Terminal = re.match(re.compile(r'^([a-z0-9]+)+', re.I), nextData['next'])
+
+
+                    #print(leftSide, '->', nextData)
+
+                    if nextData['next'] == '':                        
+                        if leftSide == nextData['next']:
+                            if findInNextFounds(leftSide) is not -1:
+                                insertNextFound(nextData['non_terminal'], nextFounds[findInNextFounds(leftSide)])
+                        else:
+                            if leftSide != nextData['non_terminal']:
+                                toSeekAfter.append({'to_seek': leftSide, 'nexts_of': nextData['non_terminal']})
+
+
+
+                    elif next2NonTerminal:
+                        nonTerminal = next2NonTerminal.group()
+                        if nonTerminal in nullNonTerminals:
+                            toSeekAfter.append({'to_seek': leftSide, 'nexts_of': nonTerminal})
+                        else:
+                            if findInNextFounds(leftSide) is not -1:
+                                insertNextFound(nextData['non_terminal'], nextFounds[findInNextFounds(leftSide)])
+                            else:
+                                nextFounds.append({leftSide: firstsGroup[nextData['next']]})
+
+                        
+                            
+                    elif next2Terminal:
+                        if findInNextFounds(leftSide) is not -1:
+                            insertNextFound(leftSide, next2Terminal.group())
+                        else:
+                            nextFounds.append({leftSide: [next2Terminal.group()]})
+
+        newNextFounds = {}
+        for data in nextFounds:
+            newNextFounds.update(data)
+        nextFounds = newNextFounds
+
+
+        for seek in toSeekAfter:
+            if seek['to_seek'] in nextFounds and seek['nexts_of'] in nextFounds:
+                nextFounds[seek['to_seek']].extend(nextFounds[seek['nexts_of']])
+            elif seek['to_seek'] not in nextFounds:
+                nextFounds.update({ seek['to_seek']: nextFounds[seek['nexts_of']]})
+
+        for seek in toSeekAfter:            
+            nextFounds[seek['to_seek']].extend(nextFounds[seek['nexts_of']])
+
+            nextFounds[seek['to_seek']] = list(set(nextFounds[seek['to_seek']]))
+
+
+        return nextFounds
 
     def selectedSet(self):
         pass
